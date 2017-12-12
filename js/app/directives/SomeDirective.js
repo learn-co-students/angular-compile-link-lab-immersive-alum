5d34a03c09a77c7f480f9185b9e1b6f59655c877
AllA <allagorik2901@gmail.com>
2017-12-11 21:56:38 -0500

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -1,16 +1,25 @@
 function SomeDirective() {
-	return {
-		template: [
-			'<div>',
-				'Replace this text!',
-			'</div>'
-		].join(''),
-		link: function (scope, elem, attrs) {
-
-		}
-	}
-}
-
-angular
-	.module('app')
-	.directive('someDirective', SomeDirective);
+  return {
+    template: [
+      '<div>',
+          'Replace this text!',
+        '</div>'
+      ].join(''),
+    compile: (element, attrs) => {
+      return {
+        pre: (scope, element, attrs) => {
+          element.html('new text')
+        },
+        post: (scope, element, attrs) => {
+          element.on('click', (e) => {
+            console.log('clicked')
+          })
+        }
+      }
+    }
+    }
+  }
+  
+  angular
+    .module('app')
+    .directive('someDirective', SomeDirective);
